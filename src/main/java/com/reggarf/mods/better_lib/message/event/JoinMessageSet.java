5d69843d51e644641803b1b3f93d817a24a6a801
Anoop Singh <Reggerfgod@gmail.com>
2025-11-21 03:11:52 +0530

    #Fix 1. join message color removed hex now its int

--- src/main/java/com/reggarf/mods/better_lib/message/event/JoinMessageSet.java
+++ src/main/java/com/reggarf/mods/better_lib/message/event/JoinMessageSet.java
@@ -3,9 +3,7 @@ package com.reggarf.mods.better_lib.message.event;
 import net.minecraft.network.chat.ClickEvent;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.Style;
-import net.minecraft.network.chat.TextColor;
 import net.minecraft.server.level.ServerPlayer;
-import java.lang.reflect.Constructor;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
@@ -15,18 +13,17 @@ import static net.minecraft.network.chat.TextColor.fromRgb;
 public class JoinMessageSet {
     private final List<Component> messages = new ArrayList<>();
 
-    public JoinMessageSet addText(String text, String colorHex) {
-        messages.add(Component.literal(text).setStyle(Style.EMPTY.withColor(parseColor(colorHex))));
+    public JoinMessageSet addText(String text, int colorRgb) {
+        messages.add(Component.literal(text).setStyle(Style.EMPTY.withColor(fromRgb(colorRgb))));
         return this;
     }
 
-    public JoinMessageSet addLink(String label, String url, String colorHex, String description) {
-        //ClickEvent clickEvent = createUrlClickEvent(url);
+    public JoinMessageSet addLink(String label, String url, int colorRgb, String description) {
         Component comp = Component.literal(" - ")
                 .append(Component.literal(label)
                         .setStyle(Style.EMPTY
                                 .withClickEvent(new ClickEvent.OpenUrl(URI.create(url)))
-                                .withColor(parseColor(colorHex))
+                                .withColor(fromRgb(colorRgb))
                                 .withUnderlined(true)))
                 .append(Component.literal(description != null ? " " + description : ""));
         messages.add(comp);
@@ -44,22 +41,6 @@ public class JoinMessageSet {
         }
     }
 
-    private TextColor parseColor(String hex) {
-        try {
-            if (hex == null) return fromRgb(0xFFFFFF);
-            if (hex.startsWith("#")) hex = hex.substring(1);
-            int rgb = Integer.parseInt(hex, 16);
-            return fromRgb(rgb);
-        } catch (Exception e) {
-            return fromRgb(0xFFFFFF);
-        }
-    }
-
-    /**
-     * Creates a version-safe ClickEvent for opening URLs.
-     * Uses URI-based constructor if available (1.21.5+),
-     * otherwise falls back to the string-based one.
-     */
 //    private ClickEvent createUrlClickEvent(String url) {
 //        try {
 //            // Try using the new 1.21.5+ method: ClickEvent.OpenUrl(URI)
